3dcf773cda637bbf774cb4822f2c1d54b26e770f
Romane <Romane@Apples-MacBook-Pro.local>
2025-11-01 23:27:37 +1100

    created azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -1,19 +1,19 @@
-Window.AddEventListener('DOMMContentLoaded', (event) =>{
+window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-  fetch(functionApi).then(response =>{
-    return response.json()
-  }.then(resonse =>{
-    console.log("Website called function API.");
-    count = response.count;
-    document.getElementById("counter").innerText = count;
-  }).catch(function(error){
-    console.log(error);
-  });
-  return count;
-  }
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error) {
+        console.log(error);
+    });
+    return count;
+}
